73b62448ea30ff5c1afff03699a5d32d563bd3d4
Victor Zamfir <zamfir.victor@gmail.com>
2017-03-14 23:57:37 +0200

    CRUDServiceContainer class - services as methods

---
+++ src/CRUDServiceContainer.js
@@ -0,0 +1,172 @@
+import Joi from 'joi';
+import { annotations, decorators, ServiceContainer } from 'octobus.js';
+
+const { service } = annotations;
+const { withSchema } = decorators;
+
+class CRUDServiceContainer extends ServiceContainer {
+  constructor(store, schema) {
+    super();
+    this.store = store;
+    this.schema = schema;
+  }
+
+  @service({
+    decorators: [
+      withSchema(Joi.func().required())
+    ]
+  })
+  query(fn) {
+    return fn(this.store);
+  }
+
+  @service({
+    decorators: [
+      withSchema(Joi.any().required())
+    ]
+  })
+  findById(id) {
+    return this.store.findById(id);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          query: Joi.object(),
+          options: Joi.object(),
+        }).default({}),
+      )
+    ]
+  })
+  findOne({ query, options }) {
+    return this.store.findOne({ query, options });
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          query: Joi.object(),
+          orderBy: Joi.any(),
+          limit: Joi.number(),
+          skip: Joi.number(),
+          fields: Joi.any(),
+        }).default({}),
+      ),
+    ]
+  })
+  findMany(options) {
+    return this.store.findMany(options);
+  }
+
+  @service()
+  createOne(data) {
+    return this.save(data);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.array().min(1).required(),
+      ),
+    ]
+  })
+  createMany(data) {
+    return Promise.all(data.map(this.save));
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          update: Joi.object().required(),
+        }).unknown(true).required(),
+      )
+    ]
+  })
+  updateOne(data) {
+    return this.store.updateOne(data);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          update: Joi.object().required(),
+        }).unknown(true).required(),
+      )
+    ],
+  })
+  updateMany(data) {
+    return this.store.updateMany(data);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().unknown(true).required(),
+      ),
+    ]
+  })
+  replaceOne(data) {
+    return this.save(data);
+  }
+
+  save = (data) => this.store.save(this.validate(data))
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          query: Joi.object(),
+          options: Joi.object(),
+        }),
+      ),
+    ]
+  })
+  deleteOne(data) {
+    return this.store.deleteOne(data);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          query: Joi.object(),
+          options: Joi.object(),
+        }),
+      )
+    ],
+  })
+  deleteMany(data) {
+    return this.store.deleteMany(data);
+  }
+
+  @service({
+    decorators: [
+      withSchema(
+        Joi.object().keys({
+          query: Joi.object(),
+          options: Joi.object(),
+        }),
+      )
+    ]
+  })
+  count(data) {
+    return this.store.count(data);
+  }
+
+  validate = (data) => {
+    if (!this.schema) {
+      return data;
+    }
+
+    return Joi.attempt(data, this.schema, {
+      convert: true,
+      stripUnknown: true,
+    });
+  }
+}
+
+export default CRUDServiceContainer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import generateCRUDServices from './generateCRUDServices';
 import Store from './Store';
+import CRUDServiceContainer from './CRUDServiceContainer';
 
 export {
   generateCRUDServices,
   Store,
+  CRUDServiceContainer,
 };
