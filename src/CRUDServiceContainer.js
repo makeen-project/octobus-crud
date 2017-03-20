fc644a008b89d11fb5aba3569b5a76dea0f6174a
Victor Zamfir <zamfir.victor@gmail.com>
2017-03-20 16:18:41 +0200

    fix wrong usage of withSchema decorator

--- src/CRUDServiceContainer.js
+++ src/CRUDServiceContainer.js
@@ -10,51 +10,39 @@ class CRUDServiceContainer extends ServiceContainer {
     this.schema = schema;
   }
 
-  @service({
-    decorators: [
-      withSchema(Joi.func().required())
-    ]
-  })
+  @service()
+  @withSchema(Joi.func().required())
   query(fn) {
     return fn(this.store);
   }
 
-  @service({
-    decorators: [
-      withSchema(Joi.any().required())
-    ]
-  })
+  @service()
+  @withSchema(Joi.any().required())
   findById(id) {
     return this.store.findById(id);
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          query: Joi.object(),
-          options: Joi.object(),
-        }).default({}),
-      )
-    ]
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      query: Joi.object(),
+      options: Joi.object(),
+    }).default({}),
+  )
   findOne({ query, options }) {
     return this.store.findOne({ query, options });
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          query: Joi.object(),
-          orderBy: Joi.any(),
-          limit: Joi.number(),
-          skip: Joi.number(),
-          fields: Joi.any(),
-        }).default({}),
-      ),
-    ]
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      query: Joi.object(),
+      orderBy: Joi.any(),
+      limit: Joi.number(),
+      skip: Joi.number(),
+      fields: Joi.any(),
+    }).default({}),
+  )
   findMany(options) {
     return this.store.findMany(options);
   }
@@ -75,39 +63,28 @@ class CRUDServiceContainer extends ServiceContainer {
     return Promise.all(data.map(this.save.bind(this)));
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          update: Joi.object().required(),
-        }).unknown(true).required(),
-      )
-    ]
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      update: Joi.object().required(),
+    }).unknown(true).required(),
+  )
   updateOne(data) {
     return this.store.updateOne(data);
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          update: Joi.object().required(),
-        }).unknown(true).required(),
-      )
-    ],
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      update: Joi.object().required(),
+    }).unknown(true).required(),
+  )
   updateMany(data) {
     return this.store.updateMany(data);
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().unknown(true).required(),
-      ),
-    ]
-  })
+  @service()
+  @withSchema(Joi.object().unknown(true).required())
   replaceOne(data) {
     return this.save(data);
   }
@@ -116,44 +93,35 @@ class CRUDServiceContainer extends ServiceContainer {
     return this.store.save(this.validate(data));
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          query: Joi.object(),
-          options: Joi.object(),
-        }),
-      ),
-    ]
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      query: Joi.object(),
+      options: Joi.object(),
+    }),
+  )
   deleteOne(data) {
     return this.store.deleteOne(data);
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          query: Joi.object(),
-          options: Joi.object(),
-        }),
-      )
-    ],
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      query: Joi.object(),
+      options: Joi.object(),
+    }),
+  )
   deleteMany(data) {
     return this.store.deleteMany(data);
   }
 
-  @service({
-    decorators: [
-      withSchema(
-        Joi.object().keys({
-          query: Joi.object(),
-          options: Joi.object(),
-        }),
-      )
-    ]
-  })
+  @service()
+  @withSchema(
+    Joi.object().keys({
+      query: Joi.object(),
+      options: Joi.object(),
+    }),
+  )
   count(data) {
     return this.store.count(data);
   }
