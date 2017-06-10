66eda03864fc6e4cd51c86dd4cbab2866cadaeef
Victor Zamfir <zamfir.victor@gmail.com>
2017-06-10 19:02:45 +0300

    store control hooks

--- src/CRUDServiceContainer.js
+++ src/CRUDServiceContainer.js
@@ -4,47 +4,59 @@ import { decorators, ServiceContainer } from 'octobus.js';
 const { withSchema, service } = decorators;
 
 class CRUDServiceContainer extends ServiceContainer {
-  constructor(store, schema) {
+  constructor(schema, store) {
     super();
-    this.store = store;
     this.schema = schema;
+    this.setStore(store);
+  }
+
+  setStore(store) {
+    this.store = store;
+  }
+
+  getStore() {
+    return this.store;
   }
 
   @service()
   @withSchema(Joi.func().required())
   query(fn) {
-    return fn(this.store);
+    return fn(this.getStore());
   }
 
   @service()
   @withSchema(Joi.any().required())
   findById(id) {
-    return this.store.findById(id);
+    return this.getStore().findById(id);
   }
 
   @service()
   @withSchema(
-    Joi.object().keys({
-      query: Joi.object(),
-      options: Joi.object(),
-    }).default({}),
+    Joi.object()
+      .keys({
+        query: Joi.object(),
+        options: Joi.object()
+      })
+      .default({})
   )
   findOne({ query, options }) {
-    return this.store.findOne({ query, options });
+    return this.getStore().findOne({ query, options });
   }
 
   @service()
   @withSchema(
-    Joi.object().keys({
-      query: Joi.object(),
-      orderBy: Joi.any(),
-      limit: Joi.number(),
-      skip: Joi.number(),
-      fields: Joi.any(),
-    }).default({}),
+    Joi.object()
+      .keys({
+        query: Joi.object(),
+        orderBy: Joi.any(),
+        limit: Joi.number(),
+        skip: Joi.number(),
+        fields: Joi.any()
+      })
+      .default({})
   )
   findMany(options) {
-    return this.store.findMany(options);
+    return this.getStore().findMany(options);
   }
 
   @service()
@@ -53,31 +65,35 @@ class CRUDServiceContainer extends ServiceContainer {
   }
 
   @service()
-  @withSchema(
-    Joi.array().min(1).required(),
-  )
+  @withSchema(Joi.array().min(1).required())
   createMany(data) {
     return Promise.all(data.map(this.save.bind(this)));
   }
 
   @service()
   @withSchema(
-    Joi.object().keys({
-      update: Joi.object().required(),
-    }).unknown(true).required(),
+    Joi.object()
+      .keys({
+        update: Joi.object().required()
+      })
+      .unknown(true)
+      .required()
   )
   updateOne(data) {
-    return this.store.updateOne(data);
+    return this.getStore().updateOne(data);
   }
 
   @service()
   @withSchema(
-    Joi.object().keys({
-      update: Joi.object().required(),
-    }).unknown(true).required(),
+    Joi.object()
+      .keys({
+        update: Joi.object().required()
+      })
+      .unknown(true)
+      .required()
   )
   updateMany(data) {
-    return this.store.updateMany(data);
+    return this.getStore().updateMany(data);
   }
 
   @service()
@@ -87,43 +103,43 @@ class CRUDServiceContainer extends ServiceContainer {
   }
 
   async save(data) {
-    return this.store.save(this.validate(data));
+    return this.getStore().save(this.validate(data));
   }
 
   @service()
   @withSchema(
     Joi.object().keys({
       query: Joi.object(),
-      options: Joi.object(),
-    }),
+      options: Joi.object()
+    })
   )
   deleteOne(data) {
-    return this.store.deleteOne(data);
+    return this.getStore().deleteOne(data);
   }
 
   @service()
   @withSchema(
     Joi.object().keys({
       query: Joi.object(),
-      options: Joi.object(),
-    }),
+      options: Joi.object()
+    })
   )
   deleteMany(data) {
-    return this.store.deleteMany(data);
+    return this.getStore().deleteMany(data);
   }
 
   @service()
   @withSchema(
     Joi.object().keys({
       query: Joi.object(),
-      options: Joi.object(),
-    }),
+      options: Joi.object()
+    })
   )
   count(data) {
-    return this.store.count(data);
+    return this.getStore().count(data);
   }
 
-  validate = (data) => {
+  validate = data => {
     if (!this.schema) {
       return data;
     }
@@ -132,7 +148,7 @@ class CRUDServiceContainer extends ServiceContainer {
       convert: true,
       stripUnknown: true,
     });
-  }
+  };
 }
 
 export default CRUDServiceContainer;
